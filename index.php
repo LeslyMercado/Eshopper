04540c68b77a34cc3e342db233f5acb0023e3cd9
LeslyMercado <leslymercado6667@gmail.com>
2024-11-21 13:41:16 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-	<?php
+<?php
   $BDProductos = array(0,"CERO",0,"");
  $i=0; $iProductos=1; 
  $filas=file('archivo.txt'); 
@@ -11,6 +11,7 @@
     array_push($BDProductos, $categoria);
     $iProductos++;
   }
+
   $BDAlmacen = array( 
     array(1, 2, 3, 4, 5), //El IDProducto
     array(0,10, 5,15, 3)); //Las Existencias
@@ -23,14 +24,13 @@
   $iVentas = 2; //El No. de ventas al momento
 ?>
 
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Software Punto de Venta" content="">
-    <meta name="Lesly Mercado" content="">
+    <meta name="LeslymercadoYusalindelacruz" content="">
     <title>Inicio | E-Shopper</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
@@ -58,8 +58,8 @@
 					<div class="col-sm-6">
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +52 618 3675141</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> joshua.perez@cbtis89.edu.mx</a></li>
+								<li><a href="#"><i class="fa fa-phone"></i> +52 618 284 4541</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> LeslymercadoYusalindelacruz@gmail.com</a></li>
 							</ul>
 						</div>
 					</div>
@@ -78,9 +78,9 @@
 							<a href="index.php"><img src="images/home/logo.png" alt="" /></a>
 						</div>						
 					</div>
-					<H3> SOFTWARE PUNTO DE VENTA <H3>
+					<H3>SOFTWARE PUNTO DE VENTA </H3>
 					<div class="col-sm-8">
-						POR: Joshua Perez
+						Por: LeslymercadoYusalindelacruz
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href="agregarproductos.php" target="_blank"><i class="fa fa-plus"></i>Agregar</a></li>
@@ -176,8 +176,8 @@
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Damas</a></h4>
-									<h4 class="panel-title"><a href="#">Caballeros</a></h4>
+									<h4 class="panel-title"><a href="#">DAMAS</a></h4>
+									<h4 class="panel-title"><a href="#">CABALLEROS</a></h4>
 								</div>
 							</div>
 						</div><!--/category-products-->
@@ -193,32 +193,37 @@
 					<div class="features_items"><!--features_items-->
 						<h2 class="title text-center">Catálogo de Productos</h2>
 						<?php
-					                  $n=4;
-                            for ($i=1; $i< $iProductos;$i++) {
+						            $n=4;
+                        for ($i=1; $i< $iProductos;$i++){   
                         ?>
 						<div class="col-sm-4">
 							<div class="product-image-wrapper">
 								<div class="single-products">
 										<div class="productinfo text-center">
-											<?php 
-											$img =$BDProductos[$n];
+											<?php
+											$img = $BDProductos[$n];
+											$productoB = $BDProductos[$n+1];
+											$precioB = $BDProductos[$n+2];
 											?>
-										   <img src="images/home/<?php echo $img ?>.jpg" width="210" height="180" alt="" />
-										   <h2><?php echo $BDProductos[$n+2]; ?></h2>
-										   <p><?php echo $BDProductos[$n+1];?></p>
-											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
+											<img src="images/home/<?php echo $img; ?>.jpg" width="210" height="180" alt="" />
+											<h2><?php echo $productoB; ?></h2>
+											<p><?php echo $precioB; ?></p>
+											<a href="cart.php?producto=<?php echo $productoB;?>&precio=<?php echo $precioB;?>" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
+											<h2><?php echo $productoB; ?></h2>
+											<p><?php echo $precioB; ?></p>
 										</div>
 										<div class="product-overlay">
 											<div class="overlay-content">
-												<h2><?php echo $BDProductos[$n+2];  ?></h2>
-												<p><?php echo $BDProductos [$n+1];?>Producto 1</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
+												<a href="cart.php?producto=<?php echo $productoB;?>&precio=<?php echo $precioB;?>" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
 											</div>
 										</div>
 								</div>								
 							</div>
 						</div>
-					<?php } ?>
+						<?php 
+						$n+=4;
+					} 
+					?>
 					</div><!--features_items-->
 					
 					
